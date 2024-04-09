66028c27d0c528eb0dd3c8cefb701fc2d70ea0c3
Marcos Gubitosi <marcosgubitosi@Marcoss-Air-2.fibertel.com.ar>
2024-04-09 00:03:24 -0300

    punto 2, creando db

--- db/index.js
+++ db/index.js
@@ -1,3 +1,227 @@
-datos = {}
+const datos = {
+    usuario: {
+        email:'armandorojas@gmail.com' ,
+        contraseña:'Arma2802' ,
+        fecha_nacimiento:'1987-04-28' ,
+        dni: 30789323 ,
+        foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+    },
+    productos: [
+        {
+            imagen: 'https://media.toyota.com.ar/a5b0d72b-55af-4553-b365-aca16a6a7be8.png',
+            nombre_producto: 'Toyota Corolla Cross',
+            descripcion: 'El Toyota Corolla Cross es un SUV compacto con un diseño elegante y versátil. Ofrece un interior espacioso, tecnología avanzada y opciones eficientes de motorización. Confortable en la ciudad y capaz en terrenos variados, es una opción popular para familias y aventuras urbanas.',
+            comentarios: [{
+                nombre_usuario: 'Armando',
+                texto: '¡Genial!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Me encanta este auto!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¿Aún está disponible?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            }
+        ]
+        },
+        {
+            imagen: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_2abcf17dcdc94ff1a86173be1522f430.jpg',
+            nombre_producto: 'Ford Mustang',
+            descripcion: 'El Ford Mustang es un ícono del automovilismo con un diseño deportivo y un rendimiento impresionante. Potente, elegante y emocionante, redefine la experiencia de conducción.',
+            comentarios: [{
+                nombre_usuario: 'Armando',
+                texto: '¡Qué buena oferta!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Quiero uno igual!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Excelente precio!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            }
+        ]
+        },
+        {
+            imagen: 'https://www.chevrolet.com.ar/content/dam/chevrolet/mercosur/argentina/espanol/index/cars/2019-cruze-premier/colorizer/enero-21/colorizer-preto-ouro-negro.jpg?imwidth=960',
+            nombre_producto: 'Chevrolet Cruze',
+            descripcion: 'El Chevrolet Cruze es un sedán moderno y sofisticado, con un diseño aerodinámico y tecnología avanzada. Eficiente en combustible y cómodo, es perfecto para la vida urbana.',
+            comentarios: [{
+                nombre_usuario: 'Armando',
+                texto: '¿Tiene garantía?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: 'Me encantaría hacer una prueba de manejo.',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Increíble diseño!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            }
+        ]
+        },
+        {
+            imagen:'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/xl/RT_V_954423484bfa4837a2348a759b5cf69e.jpg',
+            nombre_producto: 'Volkswagen Tiguan',
+            descripcion: 'El Volkswagen Tiguan es una SUV versátil y espaciosa, con un diseño moderno y características de seguridad avanzadas. Confortable en carretera y listo para la aventura, es el compañero perfecto para la familia.',
+            comentarios: [{
+                nombre_usuario: 'Armando',
+                texto: '¿Aceptan tarjeta de crédito?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¿Cuántos kilómetros tiene?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Me encantaría tenerlo en mi garaje!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            }
+        ]
+        },
+        {
+            imagen: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_b87005cb6e834d03bf2bb24213d13450.jpg',
+            nombre_producto:'BMW Serie 3',
+            descripcion:'El BMW Serie 3 es un sedán de lujo con un equilibrio perfecto entre rendimiento y elegancia. Con tecnología innovadora y un diseño distintivo, ofrece una experiencia de conducción incomparable.',
+            comentarios: [{
+                nombre_usuario: 'Armando',
+                texto: '¿Puedo obtener más información?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¿Tiene sistema de navegación?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Qué bonito color!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            }
+        ]
+        },
+        {
+            imagen: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/xl/RT_V_e4109132de26436da739fd86d5b715d0.jpg',
+            nombre_producto: 'Mercedes-Benz Clase C',
+            descripcion: 'El Mercedes-Benz Clase C es un sedán de lujo con un diseño elegante y características de vanguardia. Confortable y sofisticado, ofrece un rendimiento excepcional y tecnología intuitiva.',
+            comentarios: [{
+                nombre_usuario: 'Armando',
+                texto: '¿Es negociable el precio?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Me gusta mucho!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¿Tiene sistema de sonido premium?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            }
+        ]
+        },
+        {
+            imagen: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/xl/RT_V_9342358986924c01862e144a55d896a6.jpg',
+            nombre_producto: 'Audi Q5',
+            descripcion: 'El Audi Q5 es una SUV premium con un diseño impresionante y tecnología de vanguardia. Con tracción en las cuatro ruedas y un interior lujoso, ofrece una combinación perfecta de estilo y rendimiento.',
+            comentarios: [{
+                nombre_usuario: 'Armando',
+                texto: '¿Hay opciones de financiamiento?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Es exactamente lo que estaba buscando!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¿Puedo ver más fotos del interior?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            }
+        ]
+        },
+        {
+            imagen: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_10741a5538cc4106835033f1ff9007f0.jpg',
+            nombre_producto: 'Renault Kwid',
+            descripcion: 'El Renault Kwid es un compacto urbano con un diseño audaz y características prácticas. Ágil en la ciudad y eficiente en combustible, es ideal para la vida cotidiana.',
+            comentarios: [{
+                nombre_usuario: 'Armando',
+                texto: '¿Está disponible en otros colores?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Quiero uno para mí!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¿Hacen envíos a domicilio?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            }
+        ]
+        },
+        {
+            imagen:'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_13a028d0467e4ca78d0102ea504c7741.jpg',
+            nombre_producto: 'Peugeot 208',
+            descripcion: 'El Peugeot 208 es un hatchback moderno y elegante, con un diseño distintivo y tecnología avanzada. Confortable y eficiente, ofrece una experiencia de conducción dinámica.',
+            comentarios: [{
+                nombre_usuario: 'Armando',
+                texto: '¡Espectacular!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¿Cuál es el consumo de combustible?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Me encanta el modelo!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            }
+        ]
+        },
+        {
+            imagen: 'https://acroadtrip.blob.core.windows.net/catalogo-imagenes/s/RT_V_06a4c33100714be1af0e8328f7645596.webp',
+            nombre_producto: 'Nissan Versa',
+            descripcion: 'El Nissan Versa es un sedán compacto con un diseño moderno y amplio espacio interior. Equipado con tecnología inteligente y eficiencia de combustible, es una opción versátil para la conducción diaria.',
+            comentarios: [{
+                nombre_usuario: 'Armando',
+                texto: '¿Tiene sistema de frenado automático?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¿Puedo obtener un descuento por pago al contado?',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            },
+            {
+                nombre_usuario: 'Armando',
+                texto: '¡Me lo llevo!',
+                foto_perfil: 'https://i.pinimg.com/474x/70/85/54/7085548f3d0372a08aea0291ddcee895.jpg'
+            }
+        ]
+        }
+    ]
+}
 
-module.exports = datos;
+
+
+
+
+
