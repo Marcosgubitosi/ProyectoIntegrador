4e27f245eaf72b771fd97bc935cc45b850de512d
StefanoPietropaolo <spietropaolo@udesa.edu.ar>
2024-05-22 20:44:55 -0300

    procesando datos con .create

--- controllers/productController.js
+++ controllers/productController.js
@@ -20,7 +20,26 @@ const productController = {
         return res.render('product-add',{
             lista: datos
         })
-    }
+    },
+    processProductAdd: function(req, res) {
+        let form = req.body;
+
+        datos.Producto.create({
+            nombre_archivo_producto: form.imagen,
+            nombre_producto: form.nombre_producto,
+            descripcion_producto: form.descripcion,
+        })
+        .then(function(result) {
+            // console.log(result)
+            req.session.product = {
+                nombre_archivo_producto: form.imagen,
+                nombre_producto: form.nombre_producto,
+                descripcion_producto: form.descripcion,
+            };
+            return res.redirect('/product');
+        })
+        .catch(error=>console.log(error))
+    },
 }
 
 module.exports = productController;
