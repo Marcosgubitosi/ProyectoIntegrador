4e36fec35601bfe7c931ca7d99174c40e7a9135c
StefanoPietropaolo <spietropaolo@udesa.edu.ar>
2024-05-22 20:43:03 -0300

    Cambiando rutas product-add a POST

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
 const productController = require('../controllers/productController');
 
 router.get('/', productController.index)
 router.get('/searchresults', productController.searchresults)
 router.get('/todos', productController.todos)
 router.get('/add', productController.productAdd)
+router.post('/add', productController.processProductAdd)
 
 
 
