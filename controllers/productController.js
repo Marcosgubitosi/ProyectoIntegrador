43abb3827b689bb02da8e233c6fcfb50e961d331
Marcos Gubitosi <marcosgubitosi@Marcoss-MacBook-Air-2.local>
2024-06-19 13:12:30 -0300

    agregando orden descendente en comentarios

--- controllers/productController.js
+++ controllers/productController.js
@@ -13,6 +13,7 @@ const productController = {
                 {
                     association: "comentario",
                     include: [{ association: "usuario" }]
+                    
                 }
             ]
         }
